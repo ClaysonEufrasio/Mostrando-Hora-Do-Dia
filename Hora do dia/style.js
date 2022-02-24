dc283e8546d3df7cf93dc9a3a4cfc1ff7f711f86
ClaysonEufrasio <claysonrobertoeufrasio@gmail.com>
2022-02-23 23:51:47 -0300

    Finalizado

--- Hora do dia/style.js
+++ Hora do dia/style.js
@@ -4,4 +4,16 @@ function mostraHora() {
     var data = new Date()
     var hora = data.getHours()
     mensagem.innerHTML = `Agora são ${hora} horas`
+    if (hora < 12) {
+        imagem.src = './imagens/imagem de dia.png'
+        document.body.style.background = '#fde67c'
+    }
+    else if (hora < 18) {
+        imagem.src = './imagens/imagem da tarde.png'
+        document.body.style.background = '#d77359'
+    }
+    else {
+        imagem.src = './imagens/imagem da noite.png'
+        document.body.style.background = '#16232b'
+    }
 }
